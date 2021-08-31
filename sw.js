90ca99521d0b616bbda12c215318a6add7e97921
Agustinafc <agustina.fc1702@gmail.com>
2021-08-31 18:26:23 -0500

    Pre-Catching-Assets-and-Versioning :wq
    :

---
+++ sw.js
@@ -0,0 +1,41 @@
+const staticCacheName='site-static-v';
+const assets=[
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/css/styles.css',
+    'img/dish.png',
+];
+
+self.addEventListener('install', evt => {
+    //console.log('service worker installed');
+    evt.waitUntil(
+      caches.open(staticCacheName).then(cache=> {
+        console.log('caching shell assets');
+        cache.addAll(assets);
+      })
+    );
+  });
+  
+  // activate event
+  self.addEventListener('activate', evt => {
+   evt.waitUntil(
+     caches.keys().then(keys=>{
+       return Promise.all(keys
+        .filter(key=> key !== staticCacheName)
+        .map(key => caches.delete(key))
+        )
+     })
+   );
+
+  });
+  
+  // fetch event
+  self.addEventListener('fetch', evt => {
+   // console.log('fetch event', evt);
+   evt.respondWith(
+     caches.match(evt.request).then(cacheRes =>{
+       return cacheRes || fetch(evt.request);
+     })
+     );
+    });
