d1a0bc626f2fea7357147e68db10a076d7cf5baa
Angel Raul Acosta Rojas <angelr.acosta@softtekprojects.com>
2021-09-08 12:07:57 -0500

    Conexión bd e icono

--- js/db.js
+++ js/db.js
@@ -1,16 +1,23 @@
 // add new recipe
-const form = document.querySelector('form');
-form.addEventListener('submit', evt => {
-  evt.preventDefault();
+console.log('/js/db.js');
+db.collection('recipes').onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach(change =>{
+        console.log(change, change.doc.data());
+    })
+})
+
+// const form = document.querySelector('form');
+// form.addEventListener('submit', evt => {
+//   evt.preventDefault();
   
-  const recipe = {
-    name: form.title.value,
-    ingredients: form.ingredients.value
-  };
+//   const recipe = {
+//     name: form.title.value,
+//     ingredients: form.ingredients.value
+//   };
 
-  db.collection('recipes').add(recipe)
-    .catch(err => console.log(err));
+//   db.collection('recipes').add(recipe)
+//     .catch(err => console.log(err));
 
-  form.title.value = '';
-  form.ingredients.value = '';
-});
+//   form.title.value = '';
+//   form.ingredients.value = '';
+// });
